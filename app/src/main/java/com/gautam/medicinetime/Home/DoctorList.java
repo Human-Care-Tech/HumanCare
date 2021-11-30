fdbd3ff0231e2f7d63af8b5d105bb0367ad0454b
Abrar <algourabrar@gmail.com>
2021-11-30 18:54:54 +0200

    nav bottom

--- app/src/main/java/com/gautam/medicinetime/Home/DoctorList.java
+++ app/src/main/java/com/gautam/medicinetime/Home/DoctorList.java
@@ -2,31 +2,75 @@ package com.gautam.medicinetime.Home;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Doctor;
 import com.gautam.medicinetime.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DoctorList extends AppCompatActivity {
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_list);
 
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
 
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
+                        startActivity(i);
+
+                        break;
+
+                    case R.id.nav_doctors:
+                        fragment = new DoctorFragment();
+                        Intent intent = new Intent(getApplicationContext(), DoctorList.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.nav_profile:
+                        fragment = new ProfileFragment();
+                        Intent profile = new Intent(getApplicationContext(), PatientProfileActivity.class);
+                        startActivity(profile);
+                        break;
+
+                    case R.id.nav_logout:
+                        fragment = new LogoutFragment();
+                        Intent logout = new Intent(getApplicationContext(), PatientProfileActivity.class);
+                        startActivity(logout);
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+
+                return true;
+            }
+        });
 
         RecyclerView allTasksRecyclerView = findViewById(R.id.recyclerview);
         List<Doctor> docs = GetData(allTasksRecyclerView);
--- app/src/main/java/com/gautam/medicinetime/Home/PatientProfileActivity.java
+++ app/src/main/java/com/gautam/medicinetime/Home/PatientProfileActivity.java
@@ -1,22 +1,68 @@
 package com.gautam.medicinetime.Home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.amplifyframework.auth.AuthUser;
 import com.amplifyframework.core.Amplify;
 import com.gautam.medicinetime.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PatientProfileActivity extends AppCompatActivity {
 
+//    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile);
 
+//        navigationView = findViewById(R.id.bottom_navigation);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new HomeFragment()).commit();
+//        navigationView.setSelectedItemId(R.id.nav_home);
+//
+//        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                Fragment fragment = null;
+//                switch (item.getItemId()){
+//
+//                    case R.id.nav_home:
+//                        fragment = new HomeFragment();
+//                        Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
+//                        startActivity(i);
+//
+//                        break;
+//
+//                    case R.id.nav_doctors:
+//                        fragment = new DoctorFragment();
+//                        Intent intent = new Intent(getApplicationContext(), DoctorList.class);
+//                        startActivity(intent);
+//                        break;
+//
+//                    case R.id.nav_profile:
+//                        fragment = new ProfileFragment();
+//                        Intent profile = new Intent(getApplicationContext(), PatientProfileActivity.class);
+//                        startActivity(profile);
+//                        break;
+//
+//                    case R.id.nav_logout:
+//                        fragment = new LogoutFragment();
+//                        Intent logout = new Intent(getApplicationContext(), PatientProfileActivity.class);
+//                        startActivity(logout);
+//                        break;
+//                }
+//                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+//
+//                return true;
+//            }
+//        });
 
         AuthUser user = Amplify.Auth.getCurrentUser();
         System.out.println(user.getUsername()+"aloooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
